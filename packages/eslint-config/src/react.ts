0d8143c16a7a01b6705d15d5b6bedddefc7cab19
Marco Pasqualetti <marco.pasqualetti@live.com>
2025-03-15 12:00:50 +0100

    chore(deps-dev): update all

--- packages/eslint-config/src/react.ts
+++ packages/eslint-config/src/react.ts
@@ -3,14 +3,11 @@ import reactPlugin from 'eslint-plugin-react';
 
 import { ALL_JSX_EXTENSIONS_MINIMATCH } from './utils/extensions.js';
 
-// @ts-expect-error `reactPlugin.configs.flat.recommended.rules` doesn't have proper types
 const reactConfig: Linter.Config = {
   name: '@marcalexiei/eslint-config/react',
   files: [ALL_JSX_EXTENSIONS_MINIMATCH],
-  // @ts-expect-error flat is optional for types but actually is always defined
   ...reactPlugin.configs.flat.recommended,
   rules: {
-    // @ts-expect-error flat is optional for types but actually is always defined
     ...reactPlugin.configs.flat.recommended.rules,
 
     // https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-no-useless-fragment.md
