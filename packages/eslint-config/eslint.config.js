02844b8e017bee474d7afefa8db36bba1a808e0a
Marco Pasqualetti <marco.pasqualetti@live.com>
2025-07-02 07:29:07 +0200

    fix: remove @eslint/js from peerDependencies

--- packages/eslint-config/eslint.config.js
+++ packages/eslint-config/eslint.config.js
@@ -2,9 +2,6 @@ import js from '@eslint/js';
 
 /** @type {Array<import('eslint').Linter.Config>} */
 export default [
-  {
-    ignores: ['./test/**/*'],
-  },
   js.configs.recommended,
   {
     languageOptions: {
