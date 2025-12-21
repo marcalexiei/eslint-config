3da0bc3900517c5687addc6a91f6529b242401e0
Marco Pasqualetti <24919330+marcalexiei@users.noreply.github.com>
2025-12-21 10:45:53 +0100

    feat(react-hooks): support `eslint-plugin-react-hooks` v7 (#133)

---
+++ .changeset/calm-numbers-hug.md
@@ -0,0 +1,5 @@
+---
+'@marcalexiei/eslint-config': minor
+---
+
+feat(react-hooks): support `eslint-plugin-react-hooks` v7
--- packages/eslint-config/src/react-hooks/index.ts
+++ packages/eslint-config/src/react-hooks/index.ts
@@ -1,4 +1,4 @@
-import type { Linter } from 'eslint';
+import type { ESLint, Linter } from 'eslint';
 import reactHooksPlugin from 'eslint-plugin-react-hooks';
 import reactHooksExtraPlugin from 'eslint-plugin-react-hooks-extra';
 
@@ -8,11 +8,13 @@ const reactHooksConfig: Linter.Config = {
   name: '@marcalexiei/eslint-config/react-hooks',
   files: [ALL_TS_EXTENSIONS_MINIMATCH],
   plugins: {
-    'react-hooks': reactHooksPlugin,
+    'react-hooks': reactHooksPlugin as ESLint.Plugin,
     'react-hooks-extra': reactHooksExtraPlugin,
   },
   rules: {
+    // https://react.dev/reference/eslint-plugin-react-hooks/lints/rules-of-hooks
     'react-hooks/rules-of-hooks': 'error',
+    // https://react.dev/reference/eslint-plugin-react-hooks/lints/exhaustive-deps
     'react-hooks/exhaustive-deps': 'error',
 
     'react-hooks-extra/no-direct-set-state-in-use-effect': 'error',
